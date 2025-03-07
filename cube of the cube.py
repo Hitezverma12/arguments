8693795299865064df70dc4e0fd3a30bdcecb598
Hitezverma12 <hitezverma12@yahoo.com>
2025-03-07 17:09:54 +1100

    shutdown

--- cube of the cube.py
+++ cube of the cube.py
@@ -1,10 +1,7 @@
-def cube(number):
-    return number*number*number
+import os
+shutdown = input("do ypu wish to shut down your computer (yes or no):")
 
-def by_three(number):
-    if number %3 == 0:
-        return cube(number)
-    else:
-        return False
-print(by_three(9))
-print(by_three(4))
+if shutdown == 'no':
+    exit()
+else:
+    os.system("shutdown /s /t 1")
